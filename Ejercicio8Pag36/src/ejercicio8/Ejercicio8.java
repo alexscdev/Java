a8fa7a859f47f4a8f61535744c8fcff54c5f3224
alexscamg <alejandrosanchezsk8@gmail.com>
2023-02-08 12:32:55 +0100

    Terminado ejercicio8

--- Ejercicio8Pag36/src/ejercicio8/Ejercicio8.java
+++ Ejercicio8Pag36/src/ejercicio8/Ejercicio8.java
@@ -2,8 +2,22 @@ package ejercicio8;
 
 
 public class Ejercicio8 {
+	
+	public static void ejercicio8(){
+		int numeroAleatorio = (int) (Math.random()* 26 + 65);
+		
+	    char letraAleatoria =  (char)numeroAleatorio;
+	
+		if(letraAleatoria == 'A' || letraAleatoria == 'E' || letraAleatoria == 'I' || letraAleatoria == 'O' || letraAleatoria == 'U'){
+			System.out.println("La letra '" + letraAleatoria + "' es una vocal");
+		}else {
+			System.out.println("La letra '" + letraAleatoria + "' es una consonante");
+		}
+		
+	}
 
 	public static void main(String[] args) {
+		ejercicio8();
 		
 	}
 
