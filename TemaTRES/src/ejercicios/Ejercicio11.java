d1c6b00971e85f4d1474a05028607d2466ce373e
alexscamg <alejandrosanchezsk8@gmail.com>
2023-02-23 00:54:08 +0100

    ejercicio 11/12 terminado

---
+++ TemaTRES/src/ejercicios/Ejercicio11.java
@@ -0,0 +1,62 @@
+package ejercicios;
+
+
+
+public class Ejercicio11 {
+	/** Saber cuantos clientes podemos atender con la cantidad que tenemos de comida */
+	
+	static double chocos = 0;
+	static double papas = 0;
+
+
+	
+	// Metodos
+	public static void addChocos(double x) {
+		chocos += x;
+	}
+	
+	public static void addPapas(double x) {
+		papas += x;
+	}
+	
+	public static void showChocos() {
+		System.out.println("En el almacen hay "+ chocos +" kilos de choco");
+	}
+	
+    public static void showPapas() {
+    	System.out.println("En el almacen hay "+ papas +" kilos de papas");
+	}
+	
+    
+    
+public static int saberClientes() {
+		
+		int clientes = 0;
+		
+		
+		while(papas >= 1 && chocos >= 0.5) {
+			clientes ++;
+			papas -= 1.0;
+			chocos -= 0.5;
+		}
+		
+		
+		return clientes;
+	}
+
+
+	
+	public static void main(String[] args) {
+		
+		
+		addChocos(5.0);
+		addPapas(7.5);
+		
+		
+		System.out.println("Puede atender a: "+saberClientes()+" clientes");
+		
+		showPapas();
+		showChocos();
+		
+	}
+}
